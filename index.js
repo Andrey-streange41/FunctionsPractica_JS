fa6afddf0b6ff28740466307be8e29d260f704aa
Andrii <andrejzurba749@gmail.com>
2022-01-28 21:30:58 +0200

    Add plus two tusk

--- index.js
+++ index.js
@@ -45,6 +45,113 @@ function squareSolution(a, b, c)
     return ((-b) + ((b * b - 4 * a * c) * (0.5))) / (2 * a)
 
 }
+ //1. функция которая вернет сезон года в зависимости от полученного номера месяца
+ function defineSizon(month)
+ {
+     switch (month) {
+             case 'January':
+                alert("Winter")
+             break;
+             case 'February':
+                alert("Winter")
+             break;
+             case 'March':
+                alert("Spring")
+             break;
+             case 'April':
+                alert("Spring")
+             break;
+             case 'May':
+                alert("Spring")
+             break;
+             case 'June':
+                alert("Summer")
+             break;
+             case 'July':
+                alert("Summer")
+             break;
+             case 'August':
+                alert("Summer")
+             break;
+             case 'September':
+                alert("Otem")
+             break;
+             case 'October':
+                alert("Otem")
+             break;
+             case 'November':
+                alert("Otem")
+             break;
+             case 1:
+                alert("Winter")
+             break;
+             case 'January':
+                alert("Winter")
+             break;
+             case 2:
+                alert("Winter")
+             break;
+             case 3:
+                alert("Spring")
+             break;
+             case 4:
+                alert("Spring")
+             break;
+             case 5:
+                alert("Spring")
+             break;
+             case 6:
+                alert("Summer")
+             break;
+             case 7:
+                alert("Summer")
+             break;
+             case 8:
+                alert("Summer")
+             break;
+             case 9:
+                alert("Otem")
+             break;
+             case 10:
+                alert("Otem")
+             break;
+             case 11:
+                alert("Otem")
+             break;
+             case 12:
+                alert("Winter")
+             break;
+     
+         default:
+             break;
+     }
+ }
+
+ function isAdult(age, country)
+ {
+      if(country === 'Ukraine')
+      {
+         if(age >= 18)
+            alert("OK");
+         else
+            alert("You young !");
+      }
+      else if(country === 'Japan')
+      {
+            if(age >= 20)
+                  alert("OK");
+           else
+               alert("You young !");
+      }
+      else if(country === 'Egypt')
+      {
+            if(age >= 25)
+                  alert("OK");
+           else
+               alert("You young !");
+      }
+         
+ }
 
 //   alert(greet('Andrii', 21));
 //   alert(isNotEmpty("f"));
@@ -71,4 +178,9 @@ function squareSolution(a, b, c)
 //   alert("Random number equal: " + Number.parseFloat(getRandomNumber(0,10)).toFixed(0));
 
 
-  alert(squareSolution(2, 3, 4));
+//   alert(squareSolution(2, 3, 4));
+
+//   defineSizon('January');
+
+
+ 
